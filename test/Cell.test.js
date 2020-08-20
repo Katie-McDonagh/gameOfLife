0ef81275c7f7728e9dfdf741806b78d573af0c39
Katie McDonagh <mcdonagh.katie@outlook.com>
2020-08-20 18:18:05 +0100

    red test -should be initialized with a cellState

--- test/Cell.test.js
+++ test/Cell.test.js
@@ -6,5 +6,8 @@ describe('Cell', () => {
   it('should be initialized with a cellState', () => {
     const cell = new Cell(CellState.ALIVE);
     expect(cell.state).to.equal(CellState.ALIVE);
+
+    const deadCell = new Cell(CellState.DEAD);
+    expect(cell.state).to.equal(CellState.DEAD);
   });
 });
