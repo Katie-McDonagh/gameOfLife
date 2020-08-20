c064e7fb7e3cde098607224492ed6d06b0ed1916
Katie McDonagh <mcdonagh.katie@outlook.com>
2020-08-20 18:15:56 +0100

    green test -should be initialized with a cellState

--- test/Cell.test.js
+++ test/Cell.test.js
@@ -1,9 +1,10 @@
 import { expect } from 'chai';
 import CellState from '../CellState';
+import Cell from '../Cell'
 
 describe('Cell', () => {
   it('should be initialized with a cellState', () => {
-    const cell = new CellState(CellState.ALIVE);
+    const cell = new Cell(CellState.ALIVE);
     expect(cell.state).to.equal(CellState.ALIVE);
   });
 });
