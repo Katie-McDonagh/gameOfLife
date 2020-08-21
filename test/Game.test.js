965d51971de11f146acd31c3474a53beb94414a6
Katie McDonagh <mcdonagh.katie@outlook.com>
2020-08-21 14:00:30 +0100

    red test -should retrieve a cell at a given row and colum

--- test/Game.test.js
+++ test/Game.test.js
@@ -4,14 +4,17 @@ import { DEAD } from '../CellState';
 import Game from '../Game';
 import Cell from '../Cell';
 
+const deadState = [
+  [DEAD, DEAD, DEAD], 
+  [DEAD, DEAD, DEAD],
+  [DEAD, DEAD, DEAD], 
+];
+
 describe('Game of Life', () => {
+
   it('should be initialized with a given state', () => {
-    const state = [
-      [DEAD, DEAD, DEAD], 
-      [DEAD, DEAD, DEAD],
-      [DEAD, DEAD, DEAD], 
-    ];
-    const game = new Game(state);
+
+    const game = new Game(deadState);
     const cellState = [
       [new Cell(DEAD), new Cell(DEAD), new Cell(DEAD)], 
       [new Cell(DEAD), new Cell(DEAD), new Cell(DEAD)],
@@ -20,4 +23,11 @@ describe('Game of Life', () => {
 
     expect(game.state).to.deep.equal(cellState);
   });
+
+  it('should retrieve a cell at a given row and colum', () => {
+    const game = new Game(deadState);
+    const cell = game.getCell(0, 0);
+    expect(cell).to.be.instanceof(Cell)
+    expect(cell.state).to.equal(deadState[0][0]);
+  })
 });
